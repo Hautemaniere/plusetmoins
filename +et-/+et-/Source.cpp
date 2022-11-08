5e0e5f3574ab159e7f108b4415b6b46b4b3563b8
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2022-11-08 07:11:45 +0100

    First commit

---
+++ +et-/+et-/Source.cpp
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int nombreEntre = 0, nombreMystere = 0, continuerPartie = 1, choix = 0, choixNiveau = 0;
+	int MAX = 0;
+	const int MIN = 1;
+	printf("Choix du mode :\n");
+	printf("1. Facile\n");
+	printf("2. Dur\n");
+	printf("3. Tres Dur\n");
+	printf("\nChoisissez le niveau: ");
+	scanf("%d", &choixNiveau);
+	if (choixNiveau == 1)
+	{
+		MAX = 100;
+		printf("\nFacile");
+	}
+	if (choixNiveau == 2)
+	{
+		MAX = 1000;
+		printf("\nDur");
+	}
+	if (choixNiveau == 3)
+	{
+		MAX = 10000;
+		printf("\nTres Dur");
+	}
+	printf("\n1. Mode à 1 Joueur\n");
+	printf("2. Mode à 2 joueurs\n\n");
+	printf("Entrez votre choix: ");
+	scanf("%d", &choix);
+	if (choix == 1)
+	{
+		printf("\nVous avez choisis le Mode à 1 Joueur\n\n");
+		while (continuerPartie)
+		{
+			srand(time(NULL));
+			nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+			int nombreDeCoup = 1;
+			while (nombreEntre != nombreMystere)
+			{
+				printf("Quel est le nombre mystere ? : ");
+				scanf("%d", &nombreEntre);
+				if (nombreEntre < nombreMystere)
+				{
+					printf("C'est plus\n");
+				}
+				else if (nombreEntre > nombreMystere)
+				{
+					printf("C'est moins\n");
+				}
+				else
+				{
+					printf("\nBravo !!! Le nombre mystere est bien: %d et vous l'avez trouve en %d coups.\n\n", nombreMystere, nombreDeCoup);
+				}
+				nombreDeCoup++;
+			}
+			printf("Voulez vous continuer la partie ?\n\n");
+			printf("Tapez 1 pour OUI ou 0 pour NON: ");
+			scanf("%d", &continuerPartie);
+			printf("\n\n");
+		}
+	}
+	else if (choix == 2)
+	{
+		printf("\nVous avez choisis le Mode à 2 Joueurs\n\n");
+		while (continuerPartie)
+		{
+			printf("Joueur 1, Entrez votre Nombre Mystere: ");
+			scanf("%d", &nombreMystere);
+			printf("\nJoueur 2, Trouvez le Nombre Mystere\n\n");
+			int nombreDeCoup = 1;
+			while (nombreEntre != nombreMystere)
+			{
+				printf("Quel est le nombre mystere ? : ");
+				scanf("%d", &nombreEntre);
+				if (nombreEntre < nombreMystere)
+				{
+					printf("C'est plus\n");
+				}
+				else if (nombreEntre > nombreMystere)
+				{
+					printf("C'est moins\n");
+				}
+				else
+				{
+					printf("\nBravo !!! Le nombre mystere est bien: %d et vous lavez trouve en %d coups.\n\n", nombreMystere, nombreDeCoup);
+				}
+				nombreDeCoup++;
+			}
+			printf("Voulez vous continuer la partie ?\n\n");
+			printf("Tapez 1 pour OUI ou 0 pour NON: ");
+			scanf("%d", &continuerPartie);
+			printf("\n\n");
+		}
+	}
+	return 0;
+}
